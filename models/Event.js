47535dc6195b7312187e484c448941e04cf9c311
Kamran Elchuzade <elchuzade@gmail.com>
2019-10-22 20:37:49 +0300

    added event model

---
+++ models/Event.js
@@ -0,0 +1,150 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const EventSchema = new Schema(
+  {
+    avatar: {
+      location: {
+        type: String
+      },
+      key: {
+        type: String
+      },
+      bucket: {
+        type: String
+      },
+      originalname: {
+        type: String
+      },
+      mimetype: {
+        type: String
+      },
+      size: {
+        type: Number
+      },
+      fieldName: {
+        type: String
+      }
+    },
+    category: {
+      type: String
+    },
+    title: {
+      type: String
+    },
+    intro: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    location: {
+      type: String
+    },
+    date: {
+      type: Date
+    },
+    time: {
+      type: String
+    },
+    sits: {
+      type: Number
+    },
+    price: {
+      type: Number
+    },
+    currency: {
+      type: String
+    },
+    guests: [
+      {
+        name: {
+          type: String
+        },
+        title: {
+          type: String
+        },
+        intro: {
+          type: String
+        },
+        avatar: {
+          location: {
+            type: String
+          },
+          key: {
+            type: String
+          },
+          bucket: {
+            type: String
+          },
+          originalname: {
+            type: String
+          },
+          mimetype: {
+            type: String
+          },
+          size: {
+            type: Number
+          },
+          fieldName: {
+            type: String
+          }
+        }
+      }
+    ],
+    organizers: [
+      {
+        name: {
+          type: String
+        },
+        title: {
+          type: String
+        },
+        id: {
+          type: String
+        },
+        avatar: {
+          location: {
+            type: String
+          },
+          key: {
+            type: String
+          },
+          bucket: {
+            type: String
+          },
+          originalname: {
+            type: String
+          },
+          mimetype: {
+            type: String
+          },
+          size: {
+            type: Number
+          },
+          fieldName: {
+            type: String
+          }
+        }
+      }
+    ],
+    sponsorships: [
+      {
+        title: {
+          type: String
+        },
+        features: [
+          {
+            name: {
+              type: String
+            }
+          }
+        ]
+      }
+    ]
+  },
+  { timestamps: true }
+);
+
+module.exports = Event = mongoose.model('event', EventSchema);
