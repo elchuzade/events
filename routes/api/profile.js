6d67062c10cdd1ca08b1f7f767386757b243894e
Kamran Elchuzade <elchuzade@gmail.com>
2019-10-22 23:06:40 +0300

    added update profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -18,6 +18,8 @@ aws.config.update({
 
 const s3 = new aws.S3();
 
+// @route POST api/profile/
+// @desc Update profile information
 router.post(
   '/',
   passport.authenticate('jwt', { session: false }),
@@ -49,6 +51,65 @@ router.post(
               return res.status(400).json(errors);
             });
         }
+      })
+      .catch(err => {
+        errors.profile = 'Profile not found';
+        console.log(err);
+        return res.status(404).json(errors);
+      });
+  }
+);
+
+// @route POST api/profile/avatar
+// @desc Update profile avatar
+router.post(
+  '/avatar',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        let params = {};
+        if (profile.avatar && profile.avatar.key) {
+          params = {
+            Bucket: profile.avatar.bucket,
+            Delete: {
+              Objects: [{ Key: profile.avatar.key }]
+            }
+          };
+        }
+        if (params.Delete && params.Delete.Objects.length > 0) {
+          s3.deleteObjects(params, (err, data) => {
+            if (err) console.log(err);
+          });
+        }
+        profileAvatar(req, res, err => {
+          if (err) {
+            console.log(err);
+            errors.uploadfail = 'Failed to upload an image';
+            return res.json(errors);
+          }
+          if (req.file == undefined) {
+            console.log(err);
+            errors.selectfail = 'No file selected';
+            return res.json(errors);
+          }
+          profile.avatar.location = req.file.location;
+          profile.avatar.key = req.file.key;
+          profile.avatar.bucket = req.file.bucket;
+          profile.avatar.originalname = req.file.originalname;
+          profile.avatar.mimetype = req.file.mimetype;
+          profile.avatar.size = req.file.size;
+          profile.avatar.fieldName = req.file.metadata.fieldName;
+          profile
+            .save()
+            .then(profile => res.status(201).json(profile))
+            .catch(err => {
+              console.log(err);
+              errors.profile = 'Profile can not saved';
+              return res.status(400).json(errors);
+            });
+        });
       })
       .catch(err => {
         errors.profile = 'Profile not found';
