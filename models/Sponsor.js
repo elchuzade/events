1a8f1e791c66bb9058ede522b9370b2963dcd05d
Kamran Elchuzade <elchuzade@gmail.com>
2019-10-25 23:36:29 +0300

    added a model for sponsor

---
+++ models/Sponsor.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const SponsorSchema = new Schema(
+  {
+    user: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String
+    },
+    avatar: {
+      location: {
+        type: String
+      },
+      key: {
+        type: String
+      },
+      bucket: {
+        type: String
+      },
+      originalname: {
+        type: String
+      },
+      mimetype: {
+        type: String
+      },
+      size: {
+        type: Number
+      },
+      fieldName: {
+        type: String
+      }
+    },
+    title: {
+      type: String
+    },
+    intro: {
+      type: String
+    },
+    city: {
+      type: String
+    },
+    country: {
+      type: String
+    },
+    email: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    instagram: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    phone: {
+      type: String
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = Sponsor = mongoose.model('sponsor', SponsorSchema);
